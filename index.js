a42653d9b27979394c5fad03af82a4cba14c701e
Yohannes Abera <yohannesns@gmail.com>
2018-05-24 14:17:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ function sayHiToGrandmaoGrandMa(string){
   if (string === string.toLowerCase()){
      return `${'I can\'t hear you!'}`
    }
-  // else if(string === string.toUpperCase()){
-  // return `${'YES INDEED!'}
-  //  }
+  else if(string === string.toUpperCase()){
+     return `${'YES INDEED!'}`
+   }
   else if(string === "I love you, Grandma."){
     return `${"I love you, too."}`
     }
